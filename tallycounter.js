ce618c2bdb81163088c78d804b35fce0f062f53b
Morten <3428308+robopro@users.noreply.github.com>
2019-12-09 14:30:35 +0100

    Dark mode and social sharing

--- tallycounter.js
+++ tallycounter.js
@@ -2,9 +2,8 @@ const counter = document.getElementById("counter");
 const container = document.getElementById("container");
 const reset_button = document.getElementById("reset-button");
 const copy_button = document.getElementById("copy-button");
-const success = document.getElementById("success");
-const copy_count = document.getElementById("copy-count");
-		
+const dark_slider = document.getElementById("dark-check");
+	
 container.addEventListener('click', (e) => {
 	counter.innerText = parseInt(counter.innerText) + 1;
 });
@@ -14,17 +13,43 @@ reset_button.addEventListener("click", (e) => {
 	counter.innerText = 0;
 });
 
+// COPY NUMBER AND SUCCESS FLASH
 copy_button.addEventListener("click", (e) => {
 	e.stopPropagation();
 	
-	copy_count.innerText = counter.innerText;
+	document.getElementById("copy-count").innerText = counter.innerText;
 	
 	window.getSelection().selectAllChildren(counter);
 	document.execCommand("copy");
 	window.getSelection().removeAllRanges();
 	
+	const success = document.getElementById("success");
 	success.classList.add("visible")
 	setTimeout(() => {
 		success.classList.remove("visible");
 	}, 2000);
+});
+
+// DARK MODE
+const addDark = (element) => {
+	element.classList.add("dark")
+}
+
+const removeDark = (element) => {
+	element.classList.remove("dark")
+}
+
+dark_slider.addEventListener("change", (e) => {
+	const social_sharing = document.querySelector(".social-sharing-links")
+	const footer = document.querySelector(".footer");
+
+	if (e.target.checked) {
+		addDark(container);
+		addDark(copy_button);
+		addDark(footer);
+	} else {
+		removeDark(container);
+		removeDark(copy_button);
+		removeDark(footer);	
+	}
 });
